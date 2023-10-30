4f25af6d667ac2111ea5b57878958b7902cb14f6
Akayy <techakayy@gmail.com>
2023-10-31 02:52:24 +1100

    Formatted

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
   import { heroImageUrl } from '@/utils/hero'
 </script>
-
 <template>
-  <div><span>A Fresh Start!</span><DarkModeSwitch /></div>
+  <div>
+    <span>A Fresh Start!</span>
+    <DarkModeSwitch />
+  </div>
 </template>
-
 <style></style>
