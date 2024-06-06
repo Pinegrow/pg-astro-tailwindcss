03f7bf25a23f718d261f89cbc5096e483c78489c
Akayy <techakayy@gmail.com>
2024-06-06 17:25:37 +1000

    Initial commit

---
+++ src/components/PageHome.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { heroImageUrl } from '@/utils/hero'
+</script>
+<template>
+  <div>
+    <div
+      style="
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        padding-top: 1.5rem /* 24px */;
+        padding-bottom: 1.5rem /* 24px */;
+      "
+    >
+      <DarkModeSwitch /><span style="margin-left: 0.5rem; /* 8px */"
+        >A Fresh Start!</span
+      >
+    </div>
+  </div>
+</template>
+<style></style>
